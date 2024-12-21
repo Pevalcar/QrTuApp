47f104361f89f3617a80126fe6d7325e5fb67823
Pevalcar <42533773+Pevalcar@users.noreply.github.com>
2024-12-21 12:08:35 -0500

    rss add

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from "@astrojs/rss";
+import { dataGlobal } from "@utils/dataGlobal";
+
+export function GET(context) {
+  return rss({
+    // `<title>` campo en el xml generado
+    title: dataGlobal.title,
+    // `<description>` campo en el xml generado
+    description: dataGlobal.description,
+    // Usa el "site" desde el contexto del endpoint
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: context.site,
+    // Array de `<item>`s en el xml generado
+    // Consulta la sección "Generando `items`" para ejemplos utilizando colecciones de contenido y glob imports
+    items: [],
+    // (opcional) inyecta xml personalizado
+    customData: `<language>es</language>`,
+  });
+}
--- src/utils/dataGlobal.ts
+++ src/utils/dataGlobal.ts
@@ -1,4 +1,6 @@
 export const dataGlobal = {
   title: "QRTUAPP",
   THEME_COLOR: "#ffffff",
+  description:
+    "QRTUAPP es un proyecto de código abierto para crear un QR Generator y un URL Shortener",
 };
